71ef5e6dd4403ae28a43e89258a268b2653efefd
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2022-10-03 15:02:24 +0900

    회원가입
    회원가입

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.conf import settings
 
 # Create your models here.
+class UserModel(AbstractBaseUser):
+    class Meta:
+        db_table = "my_user"
+        
+    # follow = models.ManyToManyField(settings.AUTH_UESR_MODEL,related_name='followee')
+    
